d772baa166487292b4e2a808d189aa2bc981495e
Oskar Podrucki <oskar.podrucki1@gmail.com>
2023-12-17 12:18:52 +0100

    ...

---
+++ 3.kurs.obiektowy.cpp/1.kurs.obj.cpp/farma.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Zwierze
+{
+    public:
+    string gatunek;
+    string imie;
+	int wiek;
+
+    void dodaj_zwierze()
+    {
+		cout<<endl<<"DODAWANIE NOWEGO ZWIERZECIA DO BAZY: "<<endl;
+        cout<<"Podaj gatunek: ";
+        cin>>gatunek;
+        cout<<"Podaj imie: ";
+        cin>>imie;
+        cout<<"Podaj wiek: ";
+        cin>>wiek;
+	}
+		
+    void daj_glos()
+    {
+        if (gatunek=="kot")   cout<<imie<<" lat "<<wiek<<": Miau";
+        else if (gatunek=="koza")  cout<<imie<<" lat "<<wiek<<": Bee";
+        else if (gatunek=="krowa") cout<<imie<<" lat "<<wiek<<": Muu";
+        else cout<<"nieznany gatunek";
+    }
+};
+
+int main()
+{
+    Zwierze z1;
+    z1.dodaj_zwierze();
+    z1.daj_glos();
+
+    Zwierze z2;
+    z2.dodaj_zwierze();
+    z2.daj_glos();
+
+    return 0;
+}
