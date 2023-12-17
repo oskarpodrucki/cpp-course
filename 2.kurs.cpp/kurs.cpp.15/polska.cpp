351b1456d90659152a2ecce1d6ad68999bd174b3
Oskar Podrucki <oskar.podrucki1@gmail.com>
2023-12-17 11:06:35 +0100

    ...

---
+++ 2.kurs.cpp/kurs.cpp.15/polska.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+string napis;
+
+main(){
+
+    // zmieni† typ kodowania wsz©dzie na taki sam, np. 852 je¾eli takie kodowanie ma cmd chcp 
+
+    cin>>napis;
+    cout<<"Wpisany ci¥g znak¢w: "<<napis<<endl;
+    return 0;
+}
